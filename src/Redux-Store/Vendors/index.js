3a59d216768901436088615bcf04aeef86935e1f
Alex Mitchell <alexj.mitchell94@gmail.com>
2019-11-08 12:58:05 -0800

    initial

---
+++ src/Redux-Store/Vendors/index.js
@@ -0,0 +1,1 @@
+export { default as GreetingReducer } from "./Greeting"
--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
 import React from 'react'
-import 'normalize.css/normalize.css'
 import '../styles/App.css'
 import { Provider } from 'react-redux'
-import store from '../store'
+import store from '../Redux-Store/store'
 
-import Button from './Button'
-import Greeting from './Greeting'
+
 
 export default props => {
   return (
     <Provider store={store}>
       <div>
-        <Button />
-        <Greeting />
+        <p>Stuff</p>
       </div>
     </Provider>
   )
